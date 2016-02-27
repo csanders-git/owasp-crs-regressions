c82dea92103e1808476ffd74cda46f98755a2fc4
Chaim sanders <csanders@sparsa.org>
2016-02-27 17:30:12 -0500

    Added Raw Socket Support for version switches

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import yaml
 import sys
 import os
+import socket
+import requests
 
-class Request(object):
+class TestRequest(object):
     def __init__(self,method="GET",host="localhost",url="/",version="HTTP/1.1",headers={},data="",status=200):
         self.host = host
         self.method = method
@@ -10,6 +12,7 @@ class Request(object):
         self.data = data
         self.headers = headers
         self.version = version
+
     def genCurl(self):
         command = "curl %s%s \\%s" % (self.host,self.url,os.linesep)
         command += "-X %s \\%s" % (self.method, os.linesep)
@@ -18,8 +21,29 @@ class Request(object):
                 #TODO: Escape quotes in headername and headervalue
                 command += '--header "%s: %s" \\%s' % (headerName, headerValue, os.linesep)
         command = command[:-2]
-        return command 
-        
+        return command
+
+    def rawHTTP(self):
+        CRLF = "\r\n"
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(15)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #s.setblocking(0)
+        s.connect(("www.chaimsanders.com", 80))
+        s.send("GET / HTTP/1.1%sHost: chaimsanders.com%sUser-Agent: test%s%s" % (CRLF,CRLF,CRLF,CRLF))
+        data = (s.recv(1000000))
+        print data
+        s.shutdown(1)
+        s.close()
+
+    def issueRequest(self):
+        req = requests.Request(self.method,'http://stackoverflow.com',headers=self.headers,data=self.data)
+        print req.method
+        prepared = req.prepare()
+        s = requests.Session()
+        resp = s.send(prepared)
+        print resp.status_code
+
 def returnError(errorString):
         errorString = str(errorString) + os.linesep
         sys.stderr.write(errorString)
@@ -39,14 +63,14 @@ def extractTests(doc):
             requestArgs = {}
             # if we have an empty input create default constructor
             if inputTestValues == None:
-                myReq = Request(**requestArgs)
+                myReq = TestRequest(**requestArgs)
                 continue
             # Otherwise we have input values
             for name,value in inputTestValues.iteritems():
                 requestArgs[name] = value
             try:
                 # Try to generate a request
-                myReq = Request(**requestArgs);
+                myReq = TestRequest(**requestArgs);
             except TypeError:
                 # Almost for sure they passed an invalid name, check the args of Request
                 return returnError("An invalid argument was passed to the Request constructor, check your arugments " + str(requestArgs.keys()))
@@ -68,8 +92,7 @@ def main():
         fd.close()
     myTests = extractTests(doc)
     for i in myTests:
-        print i.genCurl()
-        print
+        i.rawHTTP()
 
 if __name__ == "__main__":
     main()
