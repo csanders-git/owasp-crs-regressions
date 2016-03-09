3897ebe5b8c74b4e11e214d0af78367dbbb789d9
Chaim Sanders <csanders@sparsa.org>
2016-03-08 20:04:58 -0500

    Added requirements

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import yaml
+import yaml # We reqire pyYaml
 import sys
 import os
 import socket
@@ -12,10 +12,25 @@ class TestRequest(object):
         self.data = data
         self.headers = headers
         self.version = version
+        self.cookie = ""
+
+    def setRequestURI(self):
+        print "XYZ"
+
+    def setHeaders(self):
+        print "XYZ"
+        
+    # Cookie can be set in headers or here.
+    def setCookie(self):
+        print "XYZ"
+
+    def setData():
+        print "XYZ"
 
     def genCurl(self):
         command = "curl %s%s \\%s" % (self.host,self.url,os.linesep)
         command += "-X %s \\%s" % (self.method, os.linesep)
+        command += "--cookie %s \\%s" % (self.cookie, os.linesep)
         if(len(self.headers) != 0):
             for headerName, headerValue in self.headers.iteritems():
                 #TODO: Escape quotes in headername and headervalue
@@ -32,7 +47,7 @@ class TestRequest(object):
         s.connect(("www.chaimsanders.com", 80))
         s.send("GET / HTTP/1.1%sHost: chaimsanders.com%sUser-Agent: test%s%s" % (CRLF,CRLF,CRLF,CRLF))
         data = (s.recv(1000000))
-        print data
+        #print data
         s.shutdown(1)
         s.close()
 
@@ -78,21 +93,41 @@ def extractTests(doc):
     return myTests
 
 def main():
+    #TODO: allow for input of where directory is.
+    filePath = "."
     try:
-        # Load our YAML file
-        fd = open('test.yaml', 'r')
-    except IOError as e:
-        return returnError(str(e))
+        # Check if the path exists and we have read access
+        if(os.path.exists(filePath) and os.access(filePath,os.R_OK)):
+            pass
+        else:
+            return returnError("The YAML test folder specified could not be accessed")
+    except OSError as e:
+        return returnError("There was a problem accessing our YAML test folder. " + str(e))
+    # List all the files in that directory that are yaml files
+    # This will return either a list or error, list may be empty.
     try:
-        # Process our YAML file
-        doc = yaml.safe_load(fd)
-    except yaml.YAMLError as e:
-        return returnError(str(e))
-    finally:
-        fd.close()
-    myTests = extractTests(doc)
-    for i in myTests:
-        i.rawHTTP()
+        yamlFiles = [f for f in os.listdir(filePath) if (os.path.isfile("/".join([filePath, f])) and f[-5:] == ".yaml")]
+    except OSError as e:
+        return returnError("There was an issue listing YAML files" + str(e))
+    for testFile in yamlFiles:
+        try:
+            # Load our YAML file
+            fd = open(testFile, 'r')
+        except IOError as e:
+            return returnError(str(e))
+        try:
+            # Process our YAML file
+            doc = yaml.safe_load(fd)
+        except yaml.YAMLError as e:
+            return returnError(str(e))
+        finally:
+            fd.close()
+        myTests = extractTests(doc)
+        for i in myTests:
+            print
+            print i.genCurl()
+    #for i in myTests:
+    #    i.rawHTTP()
 
 if __name__ == "__main__":
     main()
